d6b51a7e6d15a8063ba18669e109d4e354511fd1
Jorge Zapata <jorgejaden@gmail.com>
2020-10-24 14:00:44 +0200

    testing beta 1 version

--- Burrito Company/AppDelegate.swift
+++ Burrito Company/AppDelegate.swift
@@ -9,11 +9,10 @@
 import UIKit
 import Firebase
 import GoogleSignIn
-import UserNotifications
 
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
